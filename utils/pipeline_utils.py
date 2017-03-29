ebd7cd805c9c404e7b7c1810d6c10ceb0b3a8fa1
TA <ta.animator@gmail.com>
2017-03-29 09:51:51 +0700

    add scene assembly Ad creation

--- utils/pipeline_utils.py
+++ utils/pipeline_utils.py
@@ -8,6 +8,7 @@ from startup import template
 from rftool.utils import path_info
 from rftool.utils import file_utils
 
+
 def create_asset_template(root, project, assetType, assetSubType, assetName):
 	scriptServer = os.environ['RFSCRIPT']
 	templatePath = ''
@@ -40,6 +41,9 @@ def create_asset_template(root, project, assetType, assetSubType, assetName):
 			dst = '%s/%s' % (assetPath, step)
 			result = file_utils.copyTree(src, dst)
 			logger.debug('Copy %s -> %s success' % (src, dst))
+
+		create_standin(asset)
+		repath_ad(asset)
 
 		return True
 
@@ -77,3 +81,44 @@ def create_scene_template(root, project, episodeName, sequenceName, shotName):
 			logger.debug('Copy %s -> %s success' % (src, dst))
 
 		return True
+
+
+def create_standin(asset):
+	assetPath = asset.entityPath('RFPROJECT')
+	refPath = '%s/%s' % (assetPath, config.ref)
+	files = file_utils.listFile(refPath)
+	count = 0
+
+	if files:
+		for ref in files:
+			if config.libFileReplaceKw in ref:
+				newName = ref.replace(config.libFileReplaceKw, asset.name)
+				src = '%s/%s' % (refPath, ref)
+				dst = '%s/%s' % (refPath, newName)
+				os.rename(src, dst)
+				logger.debug('Renaming %s -> %s' % (src, dst))
+				count+=1
+
+	logger.info('renaming %s files standin complete (%s)' % (count, refPath))
+
+
+def repath_ad(asset):
+	assetPath = asset.entityPath('RFPROJECT')
+	refPath = '%s/%s' % (assetPath, config.ref)
+	files = file_utils.listFile(refPath)
+	adFile = asset.libName('', 'ad', project=False)
+	replaceDict = {config.assemblyMap['assetName']: '%s_ad' % asset.name, config.assemblyMap['locatorLabel']: asset.name}
+
+	if os.path.exists('%s/%s' % (refPath, adFile)):
+
+		if files:
+			for k, v in config.representationMap.iteritems():
+				replaceFile = [a for a in files if k in a]
+
+				if replaceFile:
+					path = '%s/%s' % (refPath, replaceFile[0])
+					replaceDict.update({v: path})
+
+			logger.debug('replaceKey %s' % str(replaceDict))
+			logger.info('repath ad complete %s' % ('%s/%s' % (refPath, adFile)))
+			return file_utils.search_replace_keys('%s/%s' % (refPath, adFile), replaceDict, backupFile=False)
