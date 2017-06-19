3e5c2a0eb2335d0da40267d5dfc02a65b38f8529
taCodeTD <ta.animator@gmail.com>
2017-06-19 22:42:30 +0700

    start asset loader

--- asset_loader/loader_app.py
+++ asset_loader/loader_app.py
@@ -1,34 +1,60 @@
+# v.0.0.2 Basic UI
+
 #Import python modules
-import sys, os, re, shutil, random
+import sys, os, re, subprocess, inspect
+import platform
+import getpass
+moduleDir = os.path.dirname(sys.modules[__name__].__file__)
 
-import logging
-logger = logging.getLogger(__name__)
-logger.addHandler(logging.NullHandler())
+#Import maya commands
+try:
+    import maya.cmds as mc
+    import maya.mel as mm
+    import maya.OpenMayaUI as mui
 
-#Import GUI
-from Qt import QtCore
-from Qt import QtWidgets
-from Qt import QtGui
+    isMaya = True
+except ImportError:
+    isMaya = False
+    packagePath = '%s/python/2.7/site-packages' % os.environ['RFSCRIPT']
+    toolPath = '%s/core/maya' % os.environ['RFSCRIPT']
+    corePath = '%s/core' % os.environ['RFSCRIPT']
+    qtPath = '%s/lib/Qt.py' % os.environ['RFSCRIPT']
+    appendPaths = [packagePath, toolPath, corePath, qtPath]
 
-# print type(QtCore)
-# from QtCore import *
-# from QtWidgets import *
-# from QtGui import *
+    # add PySide lib path
+    for path in appendPaths:
+        if not path in sys.path:
+            sys.path.append(path)
 
-from Qt import wrapInstance
-from Qt import _QtUiTools
+    from startup import setEnv
+    setEnv.set()
 
+
+from functools import partial
 from startup import config
 from rftool.utils import sg_process
 from rftool.utils import get_config
 from rftool.utils import file_utils
 from rftool.utils import maya_utils
 from rftool.utils import asm_utils
+from rftool.utils import icon
 
-#Import maya commands
-import maya.cmds as mc
-import maya.mel as mm
-from functools import partial
+from rftool.utils import log_utils
+from rftool.utils.ui import load
+import logging 
+
+logFile = log_utils.name('asset_loader', user=getpass.getuser())
+logger = log_utils.init_logger(logFile)
+
+logger.setLevel(logging.INFO)
+
+
+os.environ['QT_PREFERRED_BINDING'] = os.pathsep.join(['PySide', 'PySide2'])
+from Qt import wrapInstance
+from Qt import QtCore
+from Qt import QtWidgets
+from Qt import QtGui
+
 
 module_path = sys.modules[__name__].__file__
 module_dir  = os.path.dirname(module_path)
@@ -37,18 +63,29 @@ import maya.OpenMayaUI as mui
 
 # If inside Maya open Maya GUI
 def getMayaWindow():
+    """ maya instance windows """ 
     ptr = mui.MQtUtil.mainWindow()
     return wrapInstance(long(ptr), QtWidgets.QWidget)
-    # return sip.wrapinstance(long(ptr), QObject)
+
 
 def show():
-    uiName = 'assetLoaderUI'
-    deleteUI(uiName)
-    myApp = SGAssetLoader(getMayaWindow())
-    
-    myApp.ui.project_comboBox.setCurrentIndex(21)
-    myApp.load_data()
-    myApp.ui.typeAsset_listWidget.setCurrentRow(1)
+    if isMaya:
+        logger.info('Run in Maya\n')
+        uiName = 'assetLoaderUI'
+        deleteUI(uiName)
+        myApp = SGAssetLoader(getMayaWindow())
+        # myApp.show()
+        return myApp
+
+    else:
+        logger.info('Run in standalone\n')
+        app = QtWidgets.QApplication.instance()
+        if not app: 
+            app = QtWidgets.QApplication(sys.argv)
+        myApp = SGAssetLoader()
+        # myApp.show()
+        sys.exit(app.exec_())
+
 
 def deleteUI(ui):
     if mc.window(ui, exists=True):
@@ -63,39 +100,69 @@ class SGAssetLoader(QtWidgets.QMainWindow):
         #Setup Window
         super(SGAssetLoader, self).__init__(parent)
 
+        self.server = 'server'
+        self.shotgun = 'shotgun'
+        self.mode = self.server
+
+        self.pathSep = '/'
+
         self.run_ui()
         self.initial_ui()
+        self.load_data()
         self.init_connect()
 
     def run_ui(self):
-        loader = _QtUiTools.QUiLoader()
-        file = QtCore.QFile(module_dir + "/app.ui")
-        file.open(QtCore.QFile.ReadOnly)
-        self.ui = loader.load(file, self)
-        file.close()
+        """ load ui """ 
+        uiFile = '%s/app.ui' % moduleDir
+        self.ui = load.loadUI(uiFile, self)
         self.ui.show()
+        self.setWindowTitle('RF Asset Loader v.0.0.1')
 
     def initial_ui(self):
-
         self.rootPubl = os.getenv(config.RFPUBL)
+        logger.info('Loader root has been set to %s' % self.rootPubl)
 
         self.set_project_combo()
-        self.set_level_combo()
-        self.set_resolutions_combo()
+
+        
 
     def init_connect(self):
-    	self.ui.download_pushButton.clicked.connect(self.load_data)
-        self.ui.typeAsset_listWidget.itemClicked.connect(self.list_thumbnail)
-        self.ui.subTypeAsset_listWidget.itemClicked.connect(self.list_thumbnail)
+        """ init signals """ 
+        # radioButton 
+        self.ui.shotgun_radioButton.clicked.connect(partial(self.set_mode, 'shotgun'))
+    	self.ui.server_radioButton.clicked.connect(partial(self.set_mode, 'server'))
 
-        self.ui.size_comboBox.activated.connect(self.set_grid_size)
-        self.ui.thumbnail_listWidget.customContextMenuRequested.connect(self.actionMenu)
+        self.ui.project_comboBox.currentIndexChanged.connect(self.project_trigger)
+
+        # listWidget
+        self.ui.typeAsset_listWidget.currentItemChanged.connect(self.list_subtype)
+
+        # self.ui.typeAsset_listWidget.itemClicked.connect(self.list_thumbnail)
+        # self.ui.subTypeAsset_listWidget.itemClicked.connect(self.list_thumbnail)
+
+        # self.ui.size_comboBox.activated.connect(self.set_grid_size)
+        # self.ui.thumbnail_listWidget.customContextMenuRequested.connect(self.actionMenu)
+
+
+    def set_mode(self, mode): 
+        self.mode = mode
+        self.load_data()
+
 
     def set_project_combo(self):
-        self.projects = get_config.getProjectNames()
-        #self.projects = ['AAA Game Template', 'Audio Template', 'Demo: Animation', 'Demo: Cinematic Project', 'Double_Monkey', 'ELEX Trailer', 'FOW', 'FOW:RAMAYANA', 'Film Template', 'GARUDA', 'Game Cinematic Template', 'Kaan Project', 'Kaan Template', 'Mobile Game Template', 'Motion Capture Template', 'Poul Test', 'Shotgun_Test', 'TV Series Template', 'Template Project', 'Two_Heroes', 'ZILA 2016', 'project']
+        """ read project """ 
+        lastSelected = mc.optionVar(q=config.projectVar)
         
-        self.ui.project_comboBox.addItems(self.projects)
+        if self.mode == self.server: 
+            # browse server 
+            projects = file_utils.listFolder(self.rootPubl)
+
+        self.ui.project_comboBox.addItems(projects)
+
+        if lastSelected in projects: 
+            self.ui.project_comboBox.setCurrentIndex(projects.index(lastSelected))
+
+
 
     def set_resolutions_combo(self):
         self.ui.lod_comboBox.clear()
@@ -110,6 +177,14 @@ class SGAssetLoader(QtWidgets.QMainWindow):
 
         self.ui.lod_comboBox.setCurrentIndex(2)
 
+
+    def project_trigger(self): 
+        """ project comboBox changed """ 
+        project = str(self.ui.project_comboBox.currentText())
+        mc.optionVar(sv=(config.projectVar, project))
+        self.load_data()
+
+
     def set_level_combo(self):
         self.ui.level_comboBox.clear()
         level_list = ['model', 'rig', 'shade']
@@ -121,39 +196,75 @@ class SGAssetLoader(QtWidgets.QMainWindow):
 
         self.ui.level_comboBox.setCurrentIndex(1)
 
-    def get_mode_path(self):
-        prj_name = str(self.ui.project_comboBox.currentText())
-        self.mode_path = self.rootPubl + '/' + prj_name + '/asset'
+    def get_asset_path(self):
+        """ return browsing path """ 
+        selProject = str(self.ui.project_comboBox.currentText())
+        path = self.pathSep.join([self.rootPubl, selProject, config.asset])
+        return path 
+        # self.mode_path = self.rootPubl + '/' + prj_name + '/asset'
 
     def load_data(self):
-
-        self.get_mode_path()
+        """ load data from server or shotgun """ 
+        # browsePath = self.get_asset_path()
         
-        if self.ui.shotgun_radioButton.isChecked():
-            self.types = sg_process.get_type()
-            self.subtypes = sg_process.get_subtype()
-        if self.ui.server_radioButton.isChecked():
-            self.types = get_config.allTypes(self.mode_path)
-            self.subtypes = get_config.allSubtypes(self.mode_path)
+        # if self.ui.shotgun_radioButton.isChecked():
+        #     self.types = sg_process.get_type()
+        #     self.subtypes = sg_process.get_subtype()
+
+        # if self.ui.server_radioButton.isChecked():
+        #     self.types = get_config.allTypes(browsePath)
+        #     self.subtypes = get_config.allSubtypes(browsePath)
 
         self.list_type()
-        self.list_subtype()
+        # self.list_subtype()
 
     def list_type(self):
-        self.ui.typeAsset_listWidget.clear()
-        typ_name = str(self.ui.type_lineEdit.text())
+        browsePath = self.get_asset_path()
 
-        for typ in self.types:
-            if typ_name.lower() in typ.lower():
-                self.ui.typeAsset_listWidget.addItem(typ)
+        if self.mode == self.server: 
+            assetTypes = get_config.allTypes(browsePath)
+
+        if self.mode == self.shotgun: 
+            assetTypes = sg_process.get_type()
+
+        self.ui.typeAsset_listWidget.clear()
+        filterType = str(self.ui.type_lineEdit.text())
+
+        iconWidget = QtGui.QIcon()
+        iconWidget.addPixmap(QtGui.QPixmap(icon.dir),QtGui.QIcon.Normal,QtGui.QIcon.Off)
+
+        for typ in assetTypes:
+            path = self.pathSep.join([browsePath, typ])
+            item = QtWidgets.QListWidgetItem(self.ui.typeAsset_listWidget)
+            item.setText(typ)
+            item.setData(QtCore.Qt.UserRole, path)
+            item.setIcon(iconWidget)
+
+                # self.ui.typeAsset_listWidget.addItem(typ)
 
     def list_subtype(self):
+        selTypeItem = self.ui.typeAsset_listWidget.currentItem()
         self.ui.subTypeAsset_listWidget.clear()
-        sub_name = str(self.ui.subtype_lineEdit.text())
 
-        for sub in self.subtypes:
-            if sub_name.lower() in sub.lower():
-                self.ui.subTypeAsset_listWidget.addItem(sub)
+        if selTypeItem: 
+            if self.mode == self.server: 
+                path = selTypeItem.data(QtCore.Qt.UserRole)
+                subtypes = file_utils.listFolder(path)
+
+            if self.mode == self.shotgun: 
+                subtypes = sg_process.get_subtype()
+            
+            # listSubtype 
+            iconWidget = QtGui.QIcon()
+            iconWidget.addPixmap(QtGui.QPixmap(icon.dir),QtGui.QIcon.Normal,QtGui.QIcon.Off)
+
+            for sub in subtypes:
+                subPath = self.pathSep.join([path, sub])
+                item = QtWidgets.QListWidgetItem(self.ui.subTypeAsset_listWidget)
+                item.setText(sub)
+                item.setData(QtCore.Qt.UserRole, subPath)
+                item.setIcon(iconWidget)
+
 
     def get_thumbnail(self,img_path):
         return file_utils.get_latest_file(img_path) 
@@ -165,7 +276,7 @@ class SGAssetLoader(QtWidgets.QMainWindow):
         self.sub_name = None
         self.typ_path = None
         self.sub_path = None
-        self.get_mode_path()
+        self.get_asset_path()
 
         try:
             self.typ_name = self.ui.typeAsset_listWidget.currentItem().text()
